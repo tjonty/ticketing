0fe298af5ea6fb1cdf44bdb79529af9cf390fb0a
jonty <tejanijonty2323@gmail.com>
2022-11-04 11:33:23 -0700

    change starup msg in all service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("starting up.!!");
+  console.log("Auth Service starting UP!!!!");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JET_KEY must be defined.");
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("Ticket Service starting UP!!!!");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
